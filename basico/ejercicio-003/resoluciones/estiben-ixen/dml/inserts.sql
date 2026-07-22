bcc7f47470a811d8e8913430e7499d463f4100ce
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-07-22 15:24:02 -0600

    :feat(basico): ejercicio-003

---
+++ basico/ejercicio-003/resoluciones/estiben-ixen/dml/inserts.sql
@@ -0,0 +1,16 @@
+-- Datos de practica. Puedes cambiarlos o agregar mas filas.
+USE campuslands_mysql;
+
+INSERT INTO basico_ejercicio_003 (nombre, categoria, puntaje, estado, fecha_obtenida) VALUES
+('Alpha 003', 'principal', 10, 'activo','2026-01-12'),
+('Beta 003', 'secundario', 8.02, 'revision', '2025-04-15'),
+('Gamma 003', 'principal', 6.2, 'activo','2024-06-12'),
+('gba', 'secundaria',8.9, 'inactivo','2026-05-26'),
+('Desert ', 'principal', 10, 'activo','2026-01-12'),
+('Beta 003', 'secundario', 8.02, 'revision', '2025-04-15'),
+('Gagster 003', 'principal', 6.2, 'activo','2024-06-12'),
+('gkta', 'secundaria',8.9, 'inactivo','2026-05-26'),
+('Ester 003', 'principal', 10, 'activo','2026-01-12'),
+('Ak 47', 'secundario', 8.02, 'revision', '2025-04-15'),
+('gitano 003', 'principal', 6.2, 'activo','2024-06-12'),
+('abd', 'secundaria',8.9, 'inactivo','2026-05-26');
