d12586869974a3b94981e307b9b1e24ae2137a11
00edgar <shyzedgar@gmail.com>
2026-07-22 15:15:13 -0600

    feat(basico):resolvere ejercicio-001

---
+++ basico/ejercicio-001/resoluciones/eddar-sanchez/plantilla/dml/inserts.sql
@@ -0,0 +1,6 @@
+-- Datos de practica. Puedes cambiarlos o agregar mas filas.
+USE campuslands_mysql;
+
+INSERT INTO basico_ejercicio_001 (nombre, categoria, puntaje, estado, creado_en) VALUES 
+	("Juan", "intermedio", 23, "activo", "2026-07-25"),
+    ("Marcelo", "intermedio", 23, "activo", "2026-07-25");
