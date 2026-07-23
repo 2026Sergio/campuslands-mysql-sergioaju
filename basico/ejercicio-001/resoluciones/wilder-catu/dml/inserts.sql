bb55d7d6e6f8c2192d860947dae15757a53495ea
Wilktu <fernandocolaj7@gmail.com>
2026-07-23 14:26:25 -0600

    faet(Basico): Resolucion ejercicio 1

---
+++ basico/ejercicio-001/resoluciones/wilder-catu/dml/inserts.sql
@@ -0,0 +1,13 @@
+USE campuslands_mysql;
+
+INSERT INTO equipos
+(nombre_equipo, region, puntaje, victorias, derrotas, estado)
+VALUES
+('Shadow Wolves','Norte',95,19,2,'Activo'),
+('Crimson Dragons','Sur',82,16,5,'Activo'),
+('Blue Titans','Centro',74,14,7,'Activo'),
+('Phoenix Fire','Este',69,13,8,'Activo'),
+('Night Hunters','Oeste',55,10,11,'Eliminado'),
+('Iron Legends','Norte',88,17,4,'Activo'),
+('Storm Ravens','Sur',61,11,10,'Eliminado'),
+('Golden Knights','Centro',79,15,6,'Activo');
