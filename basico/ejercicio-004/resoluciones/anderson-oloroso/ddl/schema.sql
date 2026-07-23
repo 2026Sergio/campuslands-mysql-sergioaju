f908b79676ff7da92e3468e7da521c69237fd725
Anderson-Oloroso <henrikog4@gmail.com>
2026-07-22 20:29:06 -0600

    :sparkles: Feat(basico): Resolución ejercicio 04

---
+++ basico/ejercicio-004/resoluciones/anderson-oloroso/ddl/schema.sql
@@ -0,0 +1,12 @@
+USE campuslands_mysql;
+
+CREATE TABLE IF NOT EXISTS garaje_motos (
+    moto_id INT NOT NULL AUTO_INCREMENT,
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    cilindraje INT NOT NULL,
+    anio INT NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    estado VARCHAR(30) NOT NULL,
+    PRIMARY KEY (moto_id)
+);
