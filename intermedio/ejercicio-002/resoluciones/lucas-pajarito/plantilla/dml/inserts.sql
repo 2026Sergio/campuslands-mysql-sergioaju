b3492aae8e5d767ffb700df38feef6ed4c124c7e
lucaspajarito2023-debug <lucaspajarito2023@gmail.com>
2026-07-23 19:33:32 -0600

    feat(mysql): resolver ejercicio 002-INTERMEDIO

---
+++ intermedio/ejercicio-002/resoluciones/lucas-pajarito/plantilla/dml/inserts.sql
@@ -0,0 +1,14 @@
+-- Datos de practica. Puedes cambiarlos o agregar mas filas.
+USE campuslands_mysql;
+
+INSERT INTO ranking_battle_royale (jugador, juego, victorias, puntaje, estado) VALUES
+('ShadowX', 'Fortnite', 28, 985.50, 'activo'),
+('DarkWolf', 'PUBG', 21, 874.30, 'activo'),
+('GhostFire', 'Free Fire', 35, 1125.80, 'revision'),
+('StormKing', 'Apex Legends', 18, 792.40, 'activo'),
+('NightHunter', 'Fortnite', 31, 1089.20, 'activo'),
+('VenomShot', 'PUBG', 14, 654.90, 'inactivo'),
+('BlazeX', 'Free Fire', 40, 1268.75, 'activo'),
+('IceSniper', 'Apex Legends', 26, 945.60, 'revision'),
+('ThunderPro', 'Fortnite', 19, 801.15, 'activo'),
+('FalconGG', 'PUBG', 24, 912.35, 'activo');
